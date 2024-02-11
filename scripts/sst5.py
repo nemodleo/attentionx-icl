e03c6e92196be5c0fa0cb13d65bd4d0d98a78bb3
nemodleo <nemod.leo@snu.ac.kr>
2024-02-11 20:23:50 +0900

    update scripts sst5

--- scripts/sst5.py
+++ scripts/sst5.py
@@ -16,19 +16,19 @@ def test():
 
     data = DatasetReader(dataset, input_columns=['text'], output_column='label')
 
-    tp_dict = { # acc: 0.269683257918552
-        0: "</E>Movie Review: </text>\nVery Negative",
-        1: "</E>Movie Review: </text>\nNegative",
-        2: "</E>Movie Review: </text>\nNeutral",
-        3: "</E>Movie Review: </text>\nPositive",
-        4: "</E>Movie Review: </text>\nVery Positive"
+    tp_dict = { # acc: 0.18823529411764706
+        0: "</E>Movie Review: </text> \n Very Negative",
+        1: "</E>Movie Review: </text> \n Negative",
+        2: "</E>Movie Review: </text> \n Neutral",
+        3: "</E>Movie Review: </text> \n Positive",
+        4: "</E>Movie Review: </text> \n Very Positive"
     }
-    # tp_dict = { # acc: 0.18823529411764706
-    #     0 : "</E>Movie Review: </text> \n Very Negative",
-    #     1 : "</E>Movie Review: </text> \n Negative",
-    #     2 : "</E>Movie Review: </text> \n Neutral" ,
-    #     3 : "</E>Movie Review: </text> \n Positive" ,
-    #     4 : "</E>Movie Review: </text> \n Very Positive"
+    # tp_dict = { # acc: 0.269683257918552
+    #     0: "</E>Movie Review: </text>\nVery Negative",
+    #     1: "</E>Movie Review: </text>\nNegative",
+    #     2: "</E>Movie Review: </text>\nNeutral",
+    #     3: "</E>Movie Review: </text>\nPositive",
+    #     4: "</E>Movie Review: </text>\nVery Positive"
     # }
     # tp_dict = { # acc: 0.18461538461538463
     #     0: "</E>Movie Review: </text>\nSentiment: Very Negative",
