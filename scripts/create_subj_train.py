5cdf97b56f830dfb791d8e7e78b6a2b4aff4eeb3
yoonichoi <yooni.jy.choi@gmail.com>
2024-03-09 23:39:44 +0000

    subj gptj trainset code

--- scripts/create_subj_train.py
+++ scripts/create_subj_train.py
@@ -1,5 +1,7 @@
 from datasets import Dataset, DatasetDict
-from openicl import DatasetReader
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 import json
 import vessl
 
@@ -8,28 +10,60 @@ from iclx import PromptTemplate
 from iclx import RandomRetriever
 from iclx.inferencer.parent_inferencer import ParentInferencer
 
+import numpy as np
+
+def rec_softmax(x):
+    e_x = np.exp(x)
+    e_x = 1/e_x
+    return e_x / e_x.sum(axis=0)
+
+
 vessl.init()
 
-def gen(file_path):
-    with open(file_path, 'r') as f:
-        for line in f:
-            yield json.loads(line)
+def create_data():
+    
+    def gen(file_path):
+        with open(file_path, 'r') as f:
+            for line in f:
+                yield json.loads(line)
+    
+    train_ds = Dataset.from_generator(gen, gen_kwargs={"file_path": "data/subj/mini.jsonl"})
+    val_ds = Dataset.from_generator(gen, gen_kwargs={"file_path": "data/subj/mini.jsonl"})
+    test_ds = Dataset.from_generator(gen, gen_kwargs={"file_path": "data/subj/mini.jsonl"})
+    
+    dataset_dict = DatasetDict({"train": train_ds, "validation": val_ds, "test": test_ds})
+    dataset = dataset_dict
+    
+    data = DatasetReader(dataset, input_columns=['text'], output_column='label')
+    
+    tp_dict = {
+        0 : "</E>Review: </text>\nSubjectivity: Subjective",
+        1 : "</E>Review: </text>\nSubjectivity: Objective",
+    }
+    
+    template = PromptTemplate(tp_dict, {'text': '</text>'}, ice_token='</E>')
+    retriever = RandomRetriever(data, ice_num=0)
+    inferencer = ParentInferencer(model_name='EleutherAI/gpt-j-6b')
+    predictions = inferencer.inference(retriever, ice_template=template)
+    
+    for i, p in enumerate(predictions):
+        p["text"] = dataset_dict["test"][i]["text"] 
+        p["label"] = dataset_dict["test"][i]["label"] 
+        p["label_text"] = "subjective" if dataset_dict["test"][i]["label"] == 0 else "objective"
 
-train_ds = Dataset.from_generator(gen, gen_kwargs={"file_path": "data/subj/train.jsonl"})
-val_ds = Dataset.from_generator(gen, gen_kwargs={"file_path": "data/subj/train.jsonl"})
-test_ds = Dataset.from_generator(gen, gen_kwargs={"file_path": "data/subj/train.jsonl"})
+        perplexity_values = [p[0], p[1]]
+        probabilities = rec_softmax(perplexity_values)
+    
+        p.pop(0, None)
+        p.pop(1, None)
+        p.update({str(k): v for k, v in zip(range(2), probabilities)})
+    
+    # Save predictions as file ! 
+    with open('data/subj/train_new_subj.jsonl', 'w') as f:
+        for entry in predictions:
+            json.dump(entry, f)
+            f.write('\n')
 
-dataset_dict = DatasetDict({"train": train_ds, "validation": val_ds, "test": test_ds})
-dataset = dataset_dict
 
-data = DatasetReader(dataset, input_columns=['text'], output_column='label')
-
-tp_dict = {
-    0 : "</E>Review: </text>\nSubjectivity: Subjective",
-    1 : "</E>Review: </text>\nSubjectivity: Objective",
-}
-
-template = PromptTemplate(tp_dict, {'text': '</text>'}, ice_token='</E>')
-retriever = RandomRetriever(data, ice_num=0)
-inferencer = ParentInferencer(model_name='EleutherAI/gpt-j-6b')
-predictions = inferencer.inference(retriever, ice_template=template)
+if __name__ == '__main__':
+    create_data()
