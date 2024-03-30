82ced88ea5e2c26347259055072c0390be55fed2
DHdroid <happydh1@snu.ac.kr>
2024-03-30 20:20:27 +0900

    Fix some errors

--- scripts/create_traindata_ft.py
+++ scripts/create_traindata_ft.py
@@ -2,7 +2,7 @@
 A script for generating inference data of fine-tuned model
 
 Example Usage:
-    python3 data_aug_ft_model.py \
+    python3 create_traindata_ft.py \
         --data_path ../data/sst5/train_sst5.jsonl \
         --output_path .../data/sst5/train_sst5_bert.jsonl \
         --model_name_or_path SetFit/distilbert-base-uncased__sst5__all-train
@@ -18,17 +18,17 @@ import torch.nn.functional as F
 import numpy as np
 from tqdm import tqdm
 
-device = "cuda"
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument("--device", type=str, default="cuda")
     parser.add_argument("--data_path", type=str)
     parser.add_argument("--output_path", type=str)
-    parser.add_argument("--model_name_or_path", str=str)
+    parser.add_argument("--model_name_or_path", type=str)
     parser.add_argument("--data_col", type=str, default="text")
     args = parser.parse_args()
 
-    model = AutoModelForSequenceClassification.from_pretrained(args.model_name_or_path).to(device)
+    model = AutoModelForSequenceClassification.from_pretrained(args.model_name_or_path).to(args.device)
     tkn = AutoTokenizer.from_pretrained(args.model_name_or_path)
 
     data = []
@@ -40,7 +40,7 @@ def main():
     for item in tqdm(data):
         text = item["text"]
         tokenized = tkn([text])
-        tokenized = {k: torch.tensor(tokenized[k]).to(device) for k in tokenized.keys()}
+        tokenized = {k: torch.tensor(tokenized[k]).to(args.device) for k in tokenized.keys()}
         probs = F.softmax(model(tokenized["input_ids"]).logits, dim=-1)
         item["pseudo_gt"] = torch.argmax(probs[0]).item()
         for i in range(probs.shape[1]):
