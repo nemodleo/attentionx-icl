4726669a57a0ed3f63501e87f5adf4bcda3e1c60
alan-k <alan.k@hpcnt.com>
2024-04-01 00:09:02 +0900

    Seperate datamodule

--- scripts/bert/train_sst2.py
+++ scripts/bert/train_sst2.py
@@ -1,44 +1,11 @@
 import fire
-from datasets import load_dataset
-
-from torch.utils.data import DataLoader
 
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
 
-from transformers import AutoTokenizer, AutoModelForSequenceClassification, AdamW
+from transformers import AutoModelForSequenceClassification, AdamW
 
-
-class SST2DataModule(pl.LightningDataModule):
-    def __init__(self, model_name_or_path: str, batch_size: int):
-        super().__init__()
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
-        self.batch_size = batch_size
-
-    def setup(self, stage=None):
-        dataset = load_dataset("glue", "sst2")
-        self.train_dataset, self.val_dataset = self._tokenize(dataset['train']), self._tokenize(dataset['validation'])
-
-    def _tokenize(self, dataset):
-        tokenized_dataset = dataset.map(
-            lambda examples: self.tokenizer(
-                examples['sentence'],
-                padding=True,
-                truncation=True,
-                max_length=512,
-            ),
-            batched=True,
-        )
-        return tokenized_dataset
-
-    def train_dataloader(self):
-        return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True)
-
-    def val_dataloader(self):
-        return DataLoader(self.val_dataset, batch_size=self.batch_size)
-
-    def num_labels(self):
-        return 2
+from scripts.bert.datamodule.sst2_datamodule import SST2DataModule
 
 
 class BERTTrainingModule(pl.LightningModule):
