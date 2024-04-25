5a8dc2f42fd19ad4332323ae1d4ac2c7dfea9309
nemodleo <nemod.leo@snu.ac.kr>
2024-04-25 22:27:25 +0900

    rebase

--- iclx/soft_label_generator/datamodule/mnist_text.py
+++ iclx/soft_label_generator/datamodule/mnist_text.py
@@ -1,14 +1,22 @@
 from datasets import load_dataset
 
-from iclx.soft_label_generator.datamodule.base import BaseDataModule
+from iclx.soft_label_generator.datamodule.base import BaseDataModule, BaseDataSet
 
 
 class MNISTTextDataModule(BaseDataModule):
-    def setup(self, stage=None):
+    def setup(self, stage='all'):
         dataset = load_dataset("nemodleo/mnist-text-small14")
-        self.train_dataset = self._tokenize(dataset['train'])
-        self.val_dataset = self._tokenize(dataset['test'])
-        self.test_dataset = self._tokenize(dataset['test'])
+        if stage == 'train' or stage == 'all':
+            train_dataset = dataset['train']
+            self.train_dataset = BaseDataSet(train_dataset, self.model_name_or_path, self.max_token_len)
+
+        if stage == 'validation' or stage == 'all':
+            val_dataset = dataset['test']
+            self.val_dataset = BaseDataSet(val_dataset, self.model_name_or_path, self.max_token_len)
+
+        if stage == 'train' or stage == 'all':
+            test_dataset = dataset['test']
+            self.test_dataset = BaseDataSet(test_dataset, self.model_name_or_path, self.max_token_len)
 
     def label_texts(self):
         return ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
