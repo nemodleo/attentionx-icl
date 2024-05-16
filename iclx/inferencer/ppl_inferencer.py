21134db14fcf43c4614d4d08bf815d59cb794a07
nemodleo <nemod.leo@snu.ac.kr>
2024-05-16 12:22:41 +0900

    rename inputs_next -> next_inputs

--- iclx/inferencer/ppl_inferencer.py
+++ iclx/inferencer/ppl_inferencer.py
@@ -111,14 +111,14 @@ class PPLInferencer(BaseInferencer):
             logger.info(f"Calculating PPL for prompts labeled '{labels}'")
 
             sub_prompt_label_list_dict = {}
-            sub_inputs_next_dict = {}
+            sub_next_inputs_dict = {}
             for label in labels:
                 prompt_label = retriever.get_label_and_eos(label, ice_template=ice_template, prompt_template=prompt_template)
                 add_prompt_label = ' ' + prompt_label
                 sub_prompt_label_list = [add_prompt_label] * self.batch_size
-                sub_inputs_next = self._get_inputs(sub_prompt_label_list)
+                sub_next_inputs = self._get_inputs(sub_prompt_label_list)
                 sub_prompt_label_list_dict[label] = sub_prompt_label_list
-                sub_inputs_next_dict[label] = sub_inputs_next
+                sub_next_inputs_dict[label] = sub_next_inputs
 
 
             index = 0
@@ -136,9 +136,9 @@ class PPLInferencer(BaseInferencer):
 
                     # 5.3 Get PPL
                     sub_prompt_label_list = sub_prompt_label_list_dict[label]
-                    sub_inputs_next = sub_inputs_next_dict[label]
+                    sub_next_inputs = sub_next_inputs_dict[label]
                     with torch.no_grad():
-                        sub_res = self._get_ppl_use_cache(sub_caches, inputs=sub_inputs, inputs_next=sub_inputs_next).tolist()
+                        sub_res = self._get_ppl_use_cache(sub_caches, inputs=sub_inputs, next_inputs=sub_next_inputs).tolist()
                     for res, prompt_wo_label, _add_prompt_label in zip(sub_res, sub_prompt_wo_label_list, sub_prompt_label_list):
                         sub_ppl_list.append(res)
                         prompt = prompt_wo_label + _add_prompt_label
@@ -216,18 +216,18 @@ class PPLInferencer(BaseInferencer):
                            input_texts: Optional[List[str]]=None,
                            next_texts: Optional[List[str]]=None,
                            inputs: Optional[Dict[str, torch.Tensor]]=None,
-                           inputs_next: Optional[Dict[str, torch.Tensor]]=None,
+                           next_inputs: Optional[Dict[str, torch.Tensor]]=None,
                            mask_length=None):
         if input_texts is None and inputs is None:
             raise ValueError("Either input_texts or inputs must be provided.")
-        if next_texts is None and inputs_next is None:
-            raise ValueError("Either next_texts or inputs_next must be provided.")
+        if next_texts is None and next_inputs is None:
+            raise ValueError("Either next_texts or next_inputs must be provided.")
 
         _inputs = self._get_cache(input_texts) if inputs is None else copy.deepcopy(inputs)
-        _inputs_next = self._get_inputs(next_texts) if inputs_next is None else copy.deepcopy(inputs_next)
-        _inputs_next.pop("attention_mask") #! only batch size 1
+        _next_inputs = self._get_inputs(next_texts) if next_inputs is None else copy.deepcopy(next_inputs)
+        _next_inputs.pop("attention_mask") #! only batch size 1
 
-        outputs = self.model(**_inputs_next, past_key_values=sub_caches.past_key_values)
+        outputs = self.model(**_next_inputs, past_key_values=sub_caches.past_key_values)
 
         logits = sub_caches.logits
         labels = _inputs["input_ids"]
@@ -239,7 +239,7 @@ class PPLInferencer(BaseInferencer):
 
         # add next token and logit
         logits = [torch.cat([logit, next_logit], dim=0) for logit, next_logit in zip(logits, outputs.logits)]
-        labels = [torch.cat([label, next_label], dim=0) for label, next_label in zip(labels, _inputs_next["input_ids"])]
+        labels = [torch.cat([label, next_label], dim=0) for label, next_label in zip(labels, _next_inputs["input_ids"])]
 
         # pad token and logit
         max_length = max([len(label) for label in labels])
